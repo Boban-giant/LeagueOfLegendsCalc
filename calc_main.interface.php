740bc1174eb4eb639de6d42351d90dae9ae90bd3
Boban-giant <s_qwerty@ukr.net>
2013-02-11 21:02:18 +0200

    Update calc_main.interface.php

--- calc_main.interface.php
+++ calc_main.interface.php
@@ -3,7 +3,10 @@ interface mainCalculates {
   
   function calcDamage (); // функция которая описывает логику расчётов конечного демеджа 
   function calcPhisicalDamage (); //Описывает физический дэмедж 
-  function calcMagicalDamage (); // описывает магический урон 
+  function calcMagicalDamage (); // описывает магический урон
+  
+  ///// Ссылки на механику http://na.leagueoflegends.com/board/showthread.php?t=3287   
+  ///// Механика на русском http://lol-game.ru/threads/voprosy-ob-igrovoj-mexanike.1576/#armor
 }
 
 ?>
